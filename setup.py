b80ed0fc2ebc3116692c342d5a564f2c80163c49
HEGADE <subramanyashegade@gmail.com>
2021-10-17 14:56:14 +0530

    added long description for setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name="gdcli",
-    version="1.0.6",
+    version="1.0.7",
     author="Subrahmanya s hegade",
     author_email="hegadedevelopment@gmail.com",
     description='''
     Command line too used for simple google dorks
     ''',
-
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(include=['gdcli', 'gdcli.*']),
     entry_points='''
     [console_scripts]
